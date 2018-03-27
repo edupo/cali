f0248ca9bc0f0342c92978cde6d281c58bdd86db
Eduardo Lezcano <eduardo.lezcano@be.atlascopco.com>
2018-03-27 14:59:13 +0200

    Fix script detecting distribution to switch into specific fix case.
    Signed-off-by: Eduardo Lezcano <eduardo.lezcano@be.atlascopco.com>

--- static/fix.sh
+++ static/fix.sh
@@ -4,14 +4,23 @@
 USER_ID={{.Uid}}
 GROUP_ID={{.Gid}}
 
-useradd -ms /bin/sh user
+distro=`/bin/cat /proc/version`
 
-# Change 'user' uid to host user's uid
-if [ ! -z "$USER_ID" ] && [ "$(id -u user)" != "$USER_ID" ]; then
-	# Create the user group if it does not exist
-	groupadd --non-unique -g "$GROUP_ID" group
+case $distro in
+  *Ubuntu*)
+    useradd -ms /bin/sh user
 
-	# Set the user's uid and gid
-	usermod --non-unique --uid "$USER_ID" --gid "$GROUP_ID" user
-fi
-chown -R user: /home/user
+    # Change 'user' uid to host user's uid
+    if [ ! -z "$USER_ID" ] && [ "$(id -u user)" != "$USER_ID" ]; then
+      # Create the user group if it does not exist
+      groupadd --non-unique -g "$GROUP_ID" group
+
+      # Set the user's uid and gid
+      usermod --non-unique --uid "$USER_ID" --gid "$GROUP_ID" user
+    fi
+    chown -R user: /home/user
+    ;;
+  *)
+    echo "Distro $distro is currently not supported."
+    exit 1
+esac
